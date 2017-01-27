43eb92d6d7446eb527eefa2e0cff3b2d30117521
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-27 12:09:45 -0500

    add component tests

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,12 @@
+var React     = require('react'),
+    ReactDOM  = require('react-dom'),
+    expect    = require('expect'),
+    $         = require('jQuery'),
+    TestUtils = require('react-addons-test-utils'),
+    TodoList  = require('TodoList');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect('TodoList').toExist();
+  })
+})
