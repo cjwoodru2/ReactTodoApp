96591d8d842a914620c71bf7b79d1da4e5235ab0
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-27 12:22:29 -0500

    add test to match todo's component length and todo length

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -3,10 +3,25 @@ var React     = require('react'),
     expect    = require('expect'),
     $         = require('jQuery'),
     TestUtils = require('react-addons-test-utils'),
-    TodoList  = require('TodoList');
+    TodoList  = require('TodoList'),
+    Todo      = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect('TodoList').toExist();
-  })
+  });
+
+  it('should render one Todo component for each todo Item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>)
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo)
+
+    expect(todos.length).toBe(todosComponents.length);
+  });
 })
