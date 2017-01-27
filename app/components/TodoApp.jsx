f483b3ae678fc9ec2d502269cfbdf7b4af1816bb
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-27 13:54:13 -0500

    Add search form and basic search compoenent

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,13 @@
 var React       = require('react');
 var TodoList    = require('TodoList');
 var AddTodo     = require('AddTodo');
+var TodoSearch  = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
+      showCompleted: false,
+      searchText: "",
       todos: [
         {
           id: 1,
@@ -25,6 +28,12 @@ var TodoApp = React.createClass({
       ]
     }
   },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   handleAddTodo: function (text) {
     alert('New Todo: ' + text);
 
@@ -36,7 +45,7 @@ var TodoApp = React.createClass({
       <div className="row">
         <div className="column small-centered medium-6 large-4">
           <h1>Todo App</h1>
-          <h2>Search Section</h2>
+          <TodoSearch onSearch={this.handleSearch}/>
           <TodoList todos={todos}/>
           <AddTodo onAddTodo={this.handleAddTodo}/>
         </div>
